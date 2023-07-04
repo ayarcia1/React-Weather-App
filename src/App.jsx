b9af090e61b16a034bfd76762ffde9fe89f54476
ayarcia2 <97611951+ayarcia1@users.noreply.github.com>
2023-07-04 14:59:35 -0400

    Added more weather icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import CitySearch from './components/CitySearch'
-import weatherLogo from './images/weather-icon.png'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <img src={weatherLogo} className="logo" alt="Weather Logo"/>
-      <h1>Weather Forecast</h1>
       <CitySearch />
     </>
   )
--- src/components/CitySearch.jsx
+++ src/components/CitySearch.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import Sunny from '../images/sunny.png'
+import PartlyCloudy from './../images/partly-cloudy.png'
+import Rainy from '../images/rainy.png'
+import Thunderstorm from '../images/thunderstorm.png'
+import Snowy from '../images/snowy.png'
 import './CitySearch.css';
 
 const CitySearch = () => {
@@ -45,6 +50,14 @@ const CitySearch = () => {
 
   return (
     <>
+    <div className="logo-container">
+      <img src={Sunny} className="logo logo-sunny" alt="Sunny"/>
+      <img src={PartlyCloudy} className="logo logo-partly-cloudy" alt="Partly Cloudy Logo"/>
+      <img src={Rainy} className="logo logo-rainy" alt="Rainy"/>
+      <img src={Thunderstorm} className="logo logo-thunderstorm" alt="Thunderstorm"/>
+      <img src={Snowy} className="logo logo-snowy" alt="Snowy"/>
+    </div>
+    <h1>Weather Forecast</h1>
     <div className="search-bar-container">
       <form className="search-bar-form" onSubmit={handleFormSubmit}>
         <input
